d260e7880a2970dd06700cf4c1fbe29f8f27b05d
cahhbwy <cahhbwy@mail.ustc.edu.cn>
2017-02-19 19:28:42 +0800

    USTC教务系统刷补选

---
+++ console.js
@@ -0,0 +1,15 @@
+//先登陆，打开选课界面，选择课程类型后查询，设定课程代号
+//运行一下代码进行刷课
+var kechengdaihao=new Array("103X3101");
+function buxuan(){
+	for (lesson in kechengdaihao){
+		document.getElementById("frmright").contentDocument.getElementById("dxkciframe").contentDocument.getElementById(kechengdaihao[lesson]).click();
+	}
+	return;
+}
+for (lesson in kechengdaihao){
+	document.getElementById("frmright").contentDocument.getElementById("dxkciframe").contentDocument.getElementById(kechengdaihao[lesson]).disabled=false;
+}
+bx=self.setInterval("buxuan()",1000);	//时间暂定一秒
+//运行此代码终止刷课
+window.clearInterval(bx);
