7951c35a233b53dc2a9b2ff0d043f9154b476dd9
monk <cahhbwy@mail.ustc.edu.cn>
2017-08-23 11:54:19 +0800

    增加研究生选课脚本

--- console.js
+++ console.js
@@ -1,3 +1,5 @@
+
+
 //先登陆，打开选课界面，选择课程类型后查询，设定课程代号
 //运行一下代码进行刷课
 var kechengdaihao=new Array("103X3101");
@@ -13,3 +15,13 @@ for (lesson in kechengdaihao){
 bx=self.setInterval("buxuan()",1000);	//时间暂定一秒
 //运行此代码终止刷课
 window.clearInterval(bx);
+
+
+//研究生选课 gradGetDxkc.do
+var lessons=['CS0514801'];
+var run_id=self.setInterval(function(){
+    for (lesson in lessons){
+        xk(lessons);
+    }
+},1000);
+window.clearInterval(run_id);
